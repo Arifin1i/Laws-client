6d79abc48cdc99158fc9752dfb3119a14d3e19f2
Arifin1i <108435895+Arifin1i@users.noreply.github.com>
2023-02-27 10:47:13 +0000

    reviews

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -10,33 +10,7 @@ const Services = () => {
             .then(data => setServices(data))
             // .then(data => console.log(data))
     }, [])
-
-    const handlePost = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        // const =user?.email|| 'unregistered';
-        const post = form.post.value
-
-        const review = {
-            post: post
-        }
-        console.log(review)
-
-        fetch(`http://localhost:5000/review`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(review)
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(err => {
-                console.error(err.message);
-            })
-
-    }
- 
+     
     return (
         <div>
             <p className='text-2xl font-bold '>The Services which will be provided by me</p>
@@ -50,17 +24,17 @@ const Services = () => {
                 }
             </div>
 
-            <div className='review bg-gradient-to-r from-orange-500 my-20 py-20'>
+            {/* <div className='review bg-gradient-to-r from-orange-500 my-20 py-20'>
                 <Reviews ></Reviews>
                 <h1 className='my-8 pt-4 pb-0'>do u want to submit a review ?</h1>
                 <form onSubmit={handlePost}>
-                    {/* <div className="rating">
+                    <div className="rating">
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" checked />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
-                    </div> */}
+                    </div>
                     <div>
                         <input type="text" name='post' placeholder="Type here" className="textarea textarea-bordered textarea-lg w-full max-w-3xl" />
                         <div className='justify-evenly'>
@@ -71,7 +45,7 @@ const Services = () => {
                     </div>
 
                 </form>
-            </div>
+            </div> */}
 
         </div>
     );
